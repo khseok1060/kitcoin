44b171f1d452926c7bf25b228758bf5cf8970177
Kwangho <khseok1060@gmail.com>
2018-05-12 18:25:12 +0900

    Replacing Chain and Adding a Block to the Chain

--- src/blockchain.js
+++ src/blockchain.js
@@ -24,6 +24,8 @@ const getLastBlock = () => blockchain[blockchain.length - 1];
 
 const getTimestamp = () => new Date().getTime() / 1000;
 
+const getBlockchain = () => blockchain;
+
 const createHash = (index, previousHash, timestamp, data) => 
   CryptoJS.SHA256(index + previousHash + timestamp + JSON.stringify(data)).toString();
 
@@ -85,4 +87,22 @@ const isChainValid = (candidateChain) => {
     }
   }
   return true;
+};
+
+const replaceChain = candidateChain => {
+  if(isChainValid(candidateChain) && candidateChain.length > getBlockchain().length) {
+    blockchain = candidateChain;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const addBlockToChain = candidateBlock => {
+  if(isNewBlockValid(candidateBlock, getLastBlock())) {
+    getBlockchain().push(candidateBlock);
+    return true;
+  } else {
+    return false;
+  }
 };

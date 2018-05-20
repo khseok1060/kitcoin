3f6aaa16b5b889d4a0d6076a8dfafe15c353fab9
Kwangho <khseok1060@gmail.com>
2018-05-20 11:59:31 +0900

    Validating Transaction Structure

--- src/transaction.js
+++ src/transaction.js
@@ -82,3 +82,32 @@ const updateUTxOuts = (newTxs, uTxOutList) => {
 
   return resultingUTxOuts;
 };
+
+const isTxInStructureValid = txIn => {
+
+};
+
+const isTxOutStructureValid = txOut => {
+
+};
+
+const isTxStructureValid = tx => {
+  if (typeof tx.id !== "string") {
+    console.log("Tx ID is not valid");
+    return false;
+  } else if (!(tx.txIns instanceof Array)) {
+    console.log("The TxIns are not an array");
+    return false;
+  } else if () {
+    console.log("The structure of one of the txIn is not valid");
+    return false;
+  } else if (!(tx.txOuts instanceof Array)) {
+    console.log("The TxOuts are not an array");
+    return false;
+  } else if () {
+    console.log("The structure of one of the txOut is not valid");
+    return false;
+  } else {
+    return true;
+  }
+};

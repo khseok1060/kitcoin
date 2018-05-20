7a4f16b7eb4bd4df17f2f891de7752ca1261c9bd
Kwangho <khseok1060@gmail.com>
2018-05-20 11:08:41 +0900

    Signing the Transaction

--- src/transaction.js
+++ src/transaction.js
@@ -1,4 +1,8 @@
-const CryptoJS = require("crypto-js");
+const CryptoJS = require("crypto-js"),
+  elliptic = require("elliptic"),
+  utils = require("./utils");
+
+const ec = new elliptic.ec("secp256k1");
 
 class TxOut {
   constructor(address, amount) {
@@ -21,9 +25,9 @@ class Transaction {
 }
 
 class UTxOut {
-  constructor(uTxOutId, uTxOutIndex, address, amount) {
-    this.uTxOutId = uTxOutId;
-    this.uTxOutIndex = uTxOutIndex;
+  constructor(txOutId, txOutIndex, address, amount) {
+    this.txOutId = txOutId;
+    this.txOutIndex = txOutIndex;
     this.address = address;
     this.amount = amount;
   }
@@ -33,7 +37,7 @@ let uTxOuts = [];
 
 const getTxId = tx => {
   const txInContent = tx.txIns
-    .map(txIn => txIn.uTxOutId + txIn.uTxOutIndex)
+    .map(txIn => txIn.uTxOutId + txIn.txOutIndex)
     .reduce((a, b) => a + b, "");
 
   const txOutContent = tx.txOuts
@@ -41,4 +45,20 @@ const getTxId = tx => {
     .reduce((a, b) => a + b, "");
 
   return CryptoJS.SHA256(txInContent + txOutContent).toString();
+};
+
+const findUTxOut = (txOutId, txOutIndex, uTxOutList) => {
+  return uTxOutList.find(uTxOut => uTxOut.txOutId === txOutId && uTxOut.txOutIndex === txOutIndex);
+};
+
+const signTxIn = (tx, txInIndex, privateKey, uTxOut) => {
+  const txIn = tx.txIns[txInIndex];
+  const dataToSign = tx.id;
+  const referencedUTxOut = findUTxOut(txIn.txOutId, tx.txOutIndex, uTxOuts);
+  if (referencedUTxOut === null) {
+    return;
+  }
+  const key = ec.keyFromPrivate(privateKey, "hex");
+  const signature = utils.toHexString(key.sign(dataToSign).toDER());
+  return signature;
 };

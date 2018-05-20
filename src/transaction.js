d6ce5baba5e87d9e982f51b476b0d817316f31ea
Kwangho <khseok1060@gmail.com>
2018-05-20 13:13:19 +0900

    Validating the Transaction Content

--- src/transaction.js
+++ src/transaction.js
@@ -48,7 +48,7 @@ const getTxId = tx => {
 };
 
 const findUTxOut = (txOutId, txOutIndex, uTxOutList) => {
-  return uTxOutList.find(uTxOut => uTxOut.txOutId === txOutId && uTxOut.txOutIndex === txOutIndex);
+  return uTxOutList.find(uTxO => uTxO.txOutId === txOutId && uTxO.txOutIndex === txOutIndex);
 };
 
 const signTxIn = (tx, txInIndex, privateKey, uTxOut) => {
@@ -146,6 +146,40 @@ const isTxStructureValid = tx => {
     return false;
   } else if (!tx.txOuts.map(isTxOutStructureValid).reduce((a, b) => a && b, true)) {
     console.log("The structure of one of the txOut is not valid");
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const validateTxIn = (txIn, tx, uTxOutList) => {
+  const wantedTxOut = uTxOutList.find(uTxO => uTxO.txOutId === txIn.txOutId && uTxO.txOutIndex === txIn.txOutIndex);
+  if (wantedTxOut === null) {
+    return false;
+  }
+  const address = wantedTxOut.address;
+  const key = ec.keyFromPrivate(address, "hex");
+  return key.verify(tx.id, txIn.signature);
+};
+
+const getAmountInTxIn = (txIn, uTxOutList) => findUTxOut(txIn.txOutId, txIn.txOutIndex, uTxOutList).amount;
+
+const validateTx = (tx, uTxOutList) => {
+  if(getTxId(tx) !== tx.id) {
+    return false;
+  }
+
+  const hasValidTxIns = tx.txIns.map(txIn => validateTxIn(txIn, tx, uTxOutList));
+
+  if (!hasValidTxIns) {
+    return false;
+  }
+
+  const amountInTxIns = tx.txIns.map(txIn => getAmountInTxIn(txIn, uTxOutList)).reduce((a, b) => a + b, 0);
+
+  const amountInTxOuts = tx.txOuts.map(txOut => txOut.amount).reduce((a, b) => a + b, 0);
+
+  if (amountInTxIns !== amountInTxOuts) {
     return false;
   } else {
     return true;
